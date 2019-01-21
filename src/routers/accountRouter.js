dcea11f6d10bab24022d95ebc5a2c627d174d1c3
lq527266969 <452459384@qq.com>
2019-01-21 11:18:38 +0800

    2-2 登录注册功能上传的二次尝试

---
+++ src/routers/accountRouter.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const path = require('path')
+
+//创建路由对象
+const accountRouter = express.Router()
+
+//导入控制器模块
+
+const accountController = require(path.join(__dirname,"../controllers/accountCotroller.js"))
+
+//获取注册页面的请求
+
+accountRouter.get('/register',accountController.getRegisterPage)
+
+//获取注册
+
+accountRouter.post('/register',accountController.register)
+
+
+//获取登录
+
+accountRouter.get('/login',accountController.getLoginPage)
+
+//获取验证码
+accountRouter.get('/vcode',accountController.getVcodeImg)
+
+//
+accountRouter.post('/login',accountController.login)
+//导出
+module.exports = accountRouter
+
+
