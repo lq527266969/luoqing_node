c59a1fad902d11a74c1f2993ad11e7860a9e6412
lq527266969 <452459384@qq.com>
2019-01-21 11:17:32 +0800

    2-1 注册登录功能完成

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 const express = require('express')
 
+const path = require('path')
+
+var session = require('express-session')
+
+const bodyParser = require('body-parser')
 const app =express()
 
-app.get('/',(req,res)=>{
-    res.send('hello word')
-})
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
+//设置静态根目录
+app.use(express.static(path.join(__dirname,'public')))
+//导入路由对象
+const accountRouter = require(path.join(__dirname,'routers/accountRouter.js'))
+
+app.use('/account',accountRouter)
+
+//启动
 app.listen(3000,'127.0.0.1',err=>{
     if(err){
         console.log(err);
